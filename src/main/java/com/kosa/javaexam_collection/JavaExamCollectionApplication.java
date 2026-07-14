1ff48271987ac7017400cddc5e02cc518c32d0de
kjh940222 <kjh940222@gmail.com>
2026-07-14 11:41:56 +0900

    initial commit

---
+++ src/main/java/com/kosa/javaexam_collection/JavaExamCollectionApplication.java
@@ -0,0 +1,14 @@
+package com.kosa.javaexam_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class JavaExamCollectionApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(JavaExamCollectionApplication.class, args);
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ src/test/java/com/kosa/javaexam_collection/JavaExamCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.javaexam_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
